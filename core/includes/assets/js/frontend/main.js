80ccbbdc05d4cdc163e5fe656becd4800ff2194c
Andrew Greirson <andrew@pixelkey.com.au>
2022-03-08 13:20:46 +1030

    Update javascript

--- core/includes/assets/js/frontend/main.js
+++ core/includes/assets/js/frontend/main.js
@@ -3,20 +3,31 @@
 
 pixelTooltipFollow = (el) => {
 
-    // Get location of cursor
-    let cursorX = event.clientX;
-    let cursorY = event.clientY;
+    // Get location of element
+    let elLocation = el.getBoundingClientRect();
 
-    // Get child element of tooltip
+
+    // Align this pixel-tooltip-content to the center of the element
     let tooltipContent = el.querySelector('.pixel-tooltip-content');
+    tooltipContent.style.left = `${elLocation.left + (elLocation.width / 2) - (tooltipContent.offsetWidth / 2)}px`;
 
-    // Change location of tooltip
-    tooltipContent.style.left = cursorX + 'px';
-    tooltipContent.style.top = cursorY + 'px';
+    // If tooltip is below the screen, move it up to above the element
+    if (elLocation.top < (window.innerHeight / 2)) {
+        tooltipContent.style.top = `${elLocation.top + elLocation.height + 10}px`;
+    } else {
+        tooltipContent.style.top = `${elLocation.top - tooltipContent.offsetHeight - 10}px`;
+    }
 
-    // update tooltip position when mouse moves
-    // document.addEventListener('mousemove', () => {
-    //     tooltipContent.style.left = event.clientX + 'px';
-    //     tooltipContent.style.top = event.clientY + 'px';
-    // });
+    // If tooltip content window is off the screen, compensate for that
+    if (tooltipContent.offsetLeft < 0) {
+        tooltipContent.style.left = `${0}px`;
+    } else if (tooltipContent.offsetLeft + tooltipContent.offsetWidth > window.innerWidth) {
+        tooltipContent.style.left = `${window.innerWidth - tooltipContent.offsetWidth}px`;
+    }
+
+    // If tooltip content window is wider that the screen width, compensate for that
+    if (tooltipContent.offsetWidth > window.innerWidth) {
+        tooltipContent.style.width = `${window.innerWidth * 0.9}px`;
+    }
+
 };
